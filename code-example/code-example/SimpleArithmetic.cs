fa6f1a2c2a91753aac75bfbd33bca81b1750bab4
Rob Feng <rob.feng@quest.com>
2019-09-20 15:49:48 +0800

    add base sample code

---
+++ code-example/code-example/SimpleArithmetic.cs
@@ -0,0 +1,15 @@
+﻿namespace code_example
+{
+    public class SimpleArithmetic
+    {
+        public decimal Addition(int x, int y)
+        {
+            return x + y;
+        }
+
+        public decimal Subtraction(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
